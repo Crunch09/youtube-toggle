125dbcb19f88dc37dba70d370113e76757fb03ba
Florian Thomas <flo@florianthomas.net>
2016-06-19 18:50:57 +0200

    remove console.log

--- background.js
+++ background.js
@@ -1,6 +1,5 @@
 chrome.commands.onCommand.addListener(function(command) {
   chrome.tabs.query({url: "*://*.youtube.com/*"}, function(tabs){
-    console.log(tabs);
     for(var i = 0; i < tabs.length; i++){
       chrome.tabs.executeScript(tabs[i].id, {"file":"toggle.js"});
     }
